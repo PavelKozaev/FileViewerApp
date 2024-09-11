5697a51c63795cd701a789e552bc81000d195af9
PavelKozaev <pkozaev221299@gmail.com>
2024-09-11 16:52:01 +0300

    Add models

---
+++ FileViewerApp/Models/FileDataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FileViewerApp.Models
+{
+    public class FileDataModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ FileViewerApp/Models/FileInfoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FileViewerApp.Models
+{
+    public class FileInfoModel
+    {
+        public string FileName { get; set; }
+        public long Size { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
